746d9fcd2db4134e9ec91214cfed4dd540a5b9ba
Rupert Broad <rupert.broad@gmail.com>
2019-03-01 07:51:10 +0100

    fix var names

--- notify/status_lambda.tf
+++ notify/status_lambda.tf
@@ -8,14 +8,14 @@ resource "aws_lambda_function" "codebuild" {
   role             = "${module.slack_lambda_role.role_arn}"
   handler          = "src/index.handler"
   source_code_hash = "${base64sha256(file("${path.root}/lambda/release.zip"))}"
-  runtime          = "nodejs8.10"
+  runtime          = "nodejs6.10"
   timeout          = 10
   memory_size      = 256
 
   environment {
     variables = {
-      HookUrl = "${var.slack_webhook}"
-      Channel = "${local.slack_channel}"
+      SLACK_HOOK_URL = "${var.slack_webhook}"
+      SLACK_CHANNEL  = "${local.slack_channel}"
     }
   }
 
